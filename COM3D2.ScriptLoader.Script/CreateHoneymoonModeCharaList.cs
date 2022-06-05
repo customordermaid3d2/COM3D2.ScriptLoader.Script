2bed877b500503d3758c8c12d830dcba594ea16f
lilly <lilly1987@naver.com>
2022-06-05 19:16:36 +0900

    허니문 성격 제한 해제. 테스트 안해봄

---
+++ COM3D2.ScriptLoader.Script/CreateHoneymoonModeCharaList.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using Honeymoon;
+using MaidStatus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COM3D2.ScriptLoader.Script
+{
+    public static class CreateHoneymoonModeCharaList
+    {
+        static Harmony instance;
+
+        public static void Main()
+        {
+            instance = Harmony.CreateAndPatchAll(typeof(CharacterSelectManagerSort));
+        }
+
+        public static void Unload()
+        {
+            instance?.UnpatchAll(instance.Id);
+            instance = null;
+        }
+
+        [HarmonyPatch(typeof(SceneCharacterSelect), "CreateHoneymoonModeCharaList",MethodType.StaticConstructor)]
+        [HarmonyPrefix]
+        // public static List<string> CreateHoneymoonModeCharaList()
+        public static bool SortMaidStandard1(ref List<string> __result)
+        {
+            SceneCharacterSelect.chara_guid_stock_list.Clear();
+            List<Maid> list = new List<Maid>();
+            CharacterSelectManager.DefaultMaidList(list);
+            foreach (Maid maid in list)
+            {
+                if (!maid.boNPC 
+                    //&& (maid.status.seikeiken == Seikeiken.Yes_No || maid.status.seikeiken == Seikeiken.Yes_Yes) 
+                    //&& maid.status.relation >= Relation.Lover 
+                    //&& maid.status.contract != Contract.Trainee 
+                    && HoneymoonDatabase.enabledPersonalList.Contains(maid.status.personal.id))
+                {
+                    SceneCharacterSelect.chara_guid_stock_list.Add(maid.status.guid);
+                }
+            }
+            __result= new List<string>(SceneCharacterSelect.chara_guid_stock_list);
+            return false;
+        }
+
+    }
+}
