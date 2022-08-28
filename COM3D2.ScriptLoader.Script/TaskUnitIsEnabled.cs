642f2f540b178b01c2f80c3c23be7214e54f1f9f
lilly <lilly1987@naver.com>
2022-08-28 13:07:20 +0900

    TaskUnitIsEnabled

---
+++ COM3D2.ScriptLoader.Script/TaskUnitIsEnabled.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using Honeymoon;
+using MaidStatus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace COM3D2.ScriptLoader.Script
+{
+    public static class TaskUnitIsEnabled
+    {
+        static Harmony instance;
+
+        public static void Main()
+        {
+            instance = Harmony.CreateAndPatchAll(typeof(TaskUnitIsEnabled));
+        }
+
+        public static void Unload()
+        {
+            instance?.UnpatchAll(instance.Id);
+            instance = null;
+        }
+
+        [HarmonyPatch(typeof(TaskUnit), "isEnabled", MethodType.Setter)]
+        [HarmonyPrefix]
+        // public static List<string> CreateHoneymoonModeCharaList()
+        public static void isEnabledPre(ref bool __0)
+        {
+            __0 = true;
+        }
+    }
+}
